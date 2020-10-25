cc41e8b2c20e26cab66847c7f1fdc9bbbb0091ca
lovrodijanovic <73297645+lovrodijanovic@users.noreply.github.com>
2020-10-25 16:32:16 +0100

    Create Program.cs

---
+++ DZ1/UI/Program.cs
@@ -0,0 +1,38 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace DZ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Episode ep1, ep2;
+			ep1 = new Episode();
+			ep2 = new Episode(10, 64.39, 8.7);
+			int viewers = 10;
+			for (int i = 0; i < viewers; i++)
+			{
+				ep1.AddView(GenerateRandomScore());
+				Console.WriteLine(ep1.GetMaxScore());
+			}
+			if (ep1.GetAverageScore() > ep2.GetAverageScore())
+			{
+				Console.WriteLine($"Viewers: {ep1.GetViewerCount()}");
+			}
+			else
+			{
+				Console.WriteLine($"Viewers: {ep2.GetViewerCount()}");
+			}
+		}
+
+		private static double GenerateRandomScore()
+		{
+			Random random = new Random();
+
+			double v = random.NextDouble() * 10;
+
+			return Math.Round(v, 5);
+		}
+	}
+}
