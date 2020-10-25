edb1c3509e55e24b9086ff1339a01726863dc2f8
lovrodijanovic <73297645+lovrodijanovic@users.noreply.github.com>
2020-10-25 16:20:06 +0100

    Create Class1.cs

---
+++ DZ1/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ClassLibrary1
+{
+    public class Episode
+    {
+
+        private int viewers = 0;
+
+        private double sumOfScores = 0;
+
+        private double maxScore = 0;
+
+
+        public Episode() { }
+
+        public Episode(int viewers, double sumOfScores, double maxScore)
+        {
+            this.viewers = viewers;
+            this.sumOfScores = sumOfScores;
+            this.maxScore = maxScore;
+        }
+
+        public void AddView(double score)
+        {
+            viewers++;
+            sumOfScores += score;
+
+            if (score > maxScore) maxScore = score;
+        }
+
+
+        public int GetViewerCount()
+        {
+            return viewers;
+        }
+        
+        public double GetAverageScore()
+        {
+            return sumOfScores/(double)viewers;
+        }
+
+        public double GetMaxScore()
+        {
+            return maxScore;
+        }
+    }
+}
