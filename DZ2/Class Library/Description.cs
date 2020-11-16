3eea061b176c398c838dc0c14df0e0afd231e4d3
lovrodijanovic <73297645+lovrodijanovic@users.noreply.github.com>
2020-11-16 22:50:46 +0100

    Create Description.cs

---
+++ DZ2/Class Library/Description.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Description
+    {
+        private int episode;
+        private TimeSpan duration;
+        private string EpisodeName;
+
+        public Description(int episode, TimeSpan duration, string EpisodeName)
+        {
+            this.episode = episode;
+            this.duration = duration;
+            this.EpisodeName = EpisodeName;
+        }
+
+        public override string ToString()
+        {
+            return $"{episode},{duration},{EpisodeName}";
+        }
+    }
+}
